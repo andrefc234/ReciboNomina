bc271312298850deb4fe2576e52faaa4e3800e60
andrefc234 <49660774+andrefc234@users.noreply.github.com>
2023-05-19 14:03:09 -0600

    Update VerObras.tsx

--- components/Obra/VerObras.tsx
+++ components/Obra/VerObras.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Table, Button, Modal, FormControl, FormLabel, Input, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure } from '@chakra-ui/react';
+import { Table, Button, Modal, Form, FormControl, FormLabel } from 'react-bootstrap';
 
 interface Obra {
   _id: string;
@@ -14,7 +14,7 @@ function ObrasTable() {
   const [selectedObra, setSelectedObra] = useState<Obra | null>(null);
   const [editObra, setEditObra] = useState<Obra | null>(null);
   const [deleteObra, setDeleteObra] = useState<Obra | null>(null);
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     fetch(`${process.env.IP}/api/v1/obra`)
@@ -26,13 +26,13 @@ function ObrasTable() {
   const handleEdit = (obra: Obra) => {
     setSelectedObra(obra);
     setEditObra(obra);
-    onOpen();
+    setShowModal(true);
   };
 
   const handleDelete = (obra: Obra) => {
     setSelectedObra(obra);
     setDeleteObra(obra);
-    onOpen();
+    setShowModal(true);
   };
 
   const handleSaveEdit = () => {
@@ -51,7 +51,7 @@ function ObrasTable() {
             setObras((prevObras) =>
               prevObras.map((obra) => (obra._id === data.data._id ? data.data : obra))
             );
-            onClose();
+            setShowModal(false);
           } else {
             console.error(data.error);
           }
@@ -70,7 +70,7 @@ function ObrasTable() {
           if (data.success) {
             // Remove the deleted obra from obras state
             setObras((prevObras) => prevObras.filter((obra) => obra._id !== deleteObra._id));
-            onClose();
+            setShowModal(false);
           } else {
             console.error(data.error);
           }
@@ -81,7 +81,7 @@ function ObrasTable() {
 
   return (
     <div>
-      <Table variant="striped" colorScheme="teal">
+      <Table striped bordered hover>
         <thead>
           <tr>
             <th>Obra</th>
@@ -100,11 +100,11 @@ function ObrasTable() {
                 <td>{obra.metrosC}</td>
                 <td>{obra.metrosT}</td>
                 <td>
-                  <Button colorScheme="teal" size="sm" onClick={() => handleEdit(obra)}>
-                    Edit
+                  <Button variant="primary"   className='mx-3'onClick={() => handleEdit(obra)}>
+                    Editar
                   </Button>
-                  <Button colorScheme="red" size="sm" onClick={() => handleDelete(obra)}>
-                    Delete
+                  <Button variant="danger"  onClick={() => handleDelete(obra)}>
+                    Eliminar
                   </Button>
                 </td>
               </tr>
@@ -112,53 +112,68 @@ function ObrasTable() {
         </tbody>
       </Table>
 
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Edit/Delete Obra</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            {editObra && (
-              <FormControl>
-                <FormLabel>Obra</FormLabel>
-                <Input value={editObra.obra} onChange={(e) => setEditObra({ ...editObra, obra: e.target.value })} />
-                <FormLabel>Dirección</FormLabel>
-                <Input
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Editar/Eliminar Obra</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {editObra && (
+            <Form>
+              <Form.Group>
+                <Form.Label>Obra</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={editObra.obra}
+                  onChange={(e) => setEditObra({ ...editObra, obra: e.target.value })}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Dirección</Form.Label>
+                <Form.Control
+                  type="text"
                   value={editObra.direccion}
                   onChange={(e) => setEditObra({ ...editObra, direccion: e.target.value })}
                 />
-                <FormLabel>Metros Cuadrados</FormLabel>
-                <Input
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Metros Cuadrados</Form.Label>
+                <Form.Control
+                  type="text"
                   value={editObra.metrosC}
                   onChange={(e) => setEditObra({ ...editObra, metrosC: e.target.value })}
                 />
-                <FormLabel>Metros Construcción</FormLabel>
-                <Input
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Metros Construcción</Form.Label>
+                <Form.Control
+                  type="text"
                   value={editObra.metrosT}
                   onChange={(e) => setEditObra({ ...editObra, metrosT: e.target.value })}
                 />
-              </FormControl>
-            )}
-            {deleteObra && (
-              <p>
-                Are you sure you want to delete the obra "{deleteObra.obra}" with ID {deleteObra._id}?
-              </p>
-            )}
-          </ModalBody>
-          <ModalFooter>
-            {editObra && (
-              <Button colorScheme="teal" mr={3} onClick={handleSaveEdit}>
-                Save
-              </Button>
-            )}
-            {deleteObra && (
-              <Button colorScheme="red" mr={3} onClick={handleConfirmDelete}>
-                Delete
-              </Button>
-            )}
-            <Button onClick={onClose}>Cancel</Button>
-          </ModalFooter>
-        </ModalContent>
+              </Form.Group>
+            </Form>
+          )}
+          {deleteObra && (
+            <p>
+             Seguro que quieres eliminar la obra: "{deleteObra.obra}" con el  ID {deleteObra._id}?
+            </p>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          {editObra && (
+            <Button variant="primary" onClick={handleSaveEdit}>
+              Actualizar
+            </Button>
+          )}
+          {deleteObra && (
+            <Button variant="danger" onClick={handleConfirmDelete}>
+              Eliminar
+            </Button>
+          )}
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Cancelar
+          </Button>
+        </Modal.Footer>
       </Modal>
     </div>
   );
